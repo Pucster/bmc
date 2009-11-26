46338f0f2b88237b9ef3ed410bdfa497fd24dae9
unknown <eugenc@ocalea@gmail.com>
2009-11-26 17:14:22 +0200

    Adding some files...

---
+++ footer.php
@@ -0,0 +1,9 @@
+<table width="80%" height="10%" border="0" cellspacing="0" cellpadding="0" align="middle" valign="bottom">
+<tr>
+<td width="20%"></td>
+<td></td>
+<td width="20%"><div align="right"><font size="2">Include proper message here, plx kthxbye!</font></div></td>
+</tr>
+</table>
+</body>
+</html>
