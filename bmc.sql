9bfe39ec1de22c81dd4e297388cd5ce977830ace
unknown <Cocalea@.synygy.net>
2009-11-26 21:06:06 +0200

    grr

--- bmc.sql
+++ bmc.sql
@@ -7,7 +7,7 @@
 # Server OS:                    Win32
 # Target compatibility:         ANSI SQL
 # HeidiSQL version:             4.0
-# Date/time:                    11/26/2009 6:23:10 PM
+# Date/time:                    11/26/2009 9:05:43 PM
 # --------------------------------------------------------
 
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ANSI,NO_BACKSLASH_ESCAPES';*/
@@ -18,8 +18,7 @@
 # Database structure for database 'bmc'
 #
 
-DROP DATABASE IF EXISTS "bmc";
-CREATE DATABASE "bmc" /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ "bmc" /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE "bmc";
 
@@ -28,7 +27,7 @@ USE "bmc";
 # Table structure for table 'albums'
 #
 
-CREATE TABLE "albums" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "albums" (
   "id" tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   "name" varchar(50) NOT NULL,
   "description" varchar(250) DEFAULT NULL,
@@ -53,7 +52,7 @@ CREATE TABLE "albums" (
 # Table structure for table 'categories'
 #
 
-CREATE TABLE "categories" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "categories" (
   "id" tinyint(3) unsigned NOT NULL,
   "name" varchar(50) NOT NULL,
   UNIQUE KEY "id" ("id"),
@@ -68,7 +67,7 @@ CREATE TABLE "categories" (
 
 LOCK TABLES "categories" WRITE;
 /*!40000 ALTER TABLE "categories" DISABLE KEYS;*/
-INSERT INTO "categories" ("id", "name") VALUES
+REPLACE INTO "categories" ("id", "name") VALUES
 	(1,'homepage_links');
 /*!40000 ALTER TABLE "categories" ENABLE KEYS;*/
 UNLOCK TABLES;
@@ -78,7 +77,7 @@ UNLOCK TABLES;
 # Table structure for table 'homepage_links'
 #
 
-CREATE TABLE "homepage_links" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "homepage_links" (
   "id" tinyint(3) unsigned NOT NULL,
   "name" varchar(50) NOT NULL,
   "link" varchar(250) NOT NULL,
@@ -95,19 +94,19 @@ CREATE TABLE "homepage_links" (
 
 LOCK TABLES "homepage_links" WRITE;
 /*!40000 ALTER TABLE "homepage_links" DISABLE KEYS;*/
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(1,'News','index.php','news');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(2,'About','about.php','about');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(3,'Members','members.php','members');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(4,'Meetings','meetings.php','meetings');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(5,'Various','various.php','various');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(6,'Forum','forum.php','forum');
-INSERT INTO "homepage_links" ("id", "name", "link", "image") VALUES
+REPLACE INTO "homepage_links" ("id", "name", "link", "image") VALUES
 	(7,'Album','album.php','album');
 /*!40000 ALTER TABLE "homepage_links" ENABLE KEYS;*/
 UNLOCK TABLES;
@@ -117,7 +116,7 @@ UNLOCK TABLES;
 # Table structure for table 'images'
 #
 
-CREATE TABLE "images" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "images" (
   "id" tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   "name" varchar(50) NOT NULL,
   "alt" varchar(50) DEFAULT NULL,
@@ -138,19 +137,53 @@ CREATE TABLE "images" (
 
 LOCK TABLES "images" WRITE;
 /*!40000 ALTER TABLE "images" DISABLE KEYS;*/
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(1,'382348365.jpg',NULL,'images',0,NULL,NULL);
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(2,'546139373.jpg',NULL,'images',0,NULL,NULL);
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(3,'546139373.jpg',NULL,'images',0,NULL,NULL);
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(17,'1259076371.jpg',NULL,'images',0,NULL,NULL);
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(18,'1259076581.JPG',NULL,'images',0,NULL,NULL);
-INSERT INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
+REPLACE INTO "images" ("id", "name", "alt", "path", "owner", "album_id", "news_id") VALUES
 	(19,'1259077264.JPG',NULL,'images',0,NULL,NULL);
 /*!40000 ALTER TABLE "images" ENABLE KEYS;*/
+UNLOCK TABLES;
+
+
+#
+# Table structure for table 'meetings'
+#
+
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "meetings" (
+  "id" tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  "year" mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY ("id"),
+  UNIQUE KEY "id" ("id"),
+  KEY "id_2" ("id")
+) AUTO_INCREMENT=6;
+
+
+
+#
+# Dumping data for table 'meetings'
+#
+
+LOCK TABLES "meetings" WRITE;
+/*!40000 ALTER TABLE "meetings" DISABLE KEYS;*/
+REPLACE INTO "meetings" ("id", "year") VALUES
+	(1,'2006');
+REPLACE INTO "meetings" ("id", "year") VALUES
+	(2,'2007');
+REPLACE INTO "meetings" ("id", "year") VALUES
+	(3,'2008');
+REPLACE INTO "meetings" ("id", "year") VALUES
+	(4,'2009');
+REPLACE INTO "meetings" ("id", "year") VALUES
+	(5,'2010');
+/*!40000 ALTER TABLE "meetings" ENABLE KEYS;*/
 UNLOCK TABLES;
 
 
@@ -158,7 +191,7 @@ UNLOCK TABLES;
 # Table structure for table 'members'
 #
 
-CREATE TABLE "members" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "members" (
   "id" tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   "name" varchar(50) NOT NULL,
   "nickname" varchar(50) DEFAULT NULL,
@@ -179,21 +212,21 @@ CREATE TABLE "members" (
 
 LOCK TABLES "members" WRITE;
 /*!40000 ALTER TABLE "members" DISABLE KEYS;*/
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(1,'Andrei Seredenciuc','Dracu','Radauti',1,'Ducati Indiana','Fondator');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(2,'Eugen Cocalea','Puc','Iasi',2,'Kawasaki Vulcan Drifter 1500','2007');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(3,'Vasile Vasilescu','D','SD',3,'DA','4332');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(4,'Ion Ionescu','DeLaBrad','dfda',4,'fdsfs','r432');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(5,'Gica Contra','Anti','dass',5,'fdasfswre','r4');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(12,'fsd','fsfs','fsfds',17,'fsfds','fdsdsfdsf');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(13,'43','fs','fsfs',18,'fsfsd','fdsfsd');
-INSERT INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
+REPLACE INTO "members" ("id", "name", "nickname", "location", "profile_image", "ride", "start_date") VALUES
 	(14,'vasile','vasea','barlad',19,'maica-sa','2000');
 /*!40000 ALTER TABLE "members" ENABLE KEYS;*/
 UNLOCK TABLES;
@@ -203,7 +236,7 @@ UNLOCK TABLES;
 # Table structure for table 'news'
 #
 
-CREATE TABLE "news" (
+CREATE TABLE /*!32312 IF NOT EXISTS*/ "news" (
   "id" tinyint(3) unsigned NOT NULL,
   "title" varchar(250) NOT NULL,
   "content" longtext NOT NULL,
@@ -220,9 +253,9 @@ CREATE TABLE "news" (
 
 LOCK TABLES "news" WRITE;
 /*!40000 ALTER TABLE "news" DISABLE KEYS;*/
-INSERT INTO "news" ("id", "title", "content", "category_id") VALUES
+REPLACE INTO "news" ("id", "title", "content", "category_id") VALUES
 	(1,'Am luat bacu','Ana are mere',1);
-INSERT INTO "news" ("id", "title", "content", "category_id") VALUES
+REPLACE INTO "news" ("id", "title", "content", "category_id") VALUES
 	(2,'Iar am luat bacu','Ana nu mai are mere!',1);
 /*!40000 ALTER TABLE "news" ENABLE KEYS;*/
 UNLOCK TABLES;
