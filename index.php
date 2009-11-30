a153f697935712a3047356d0fb0bf48c8e3dc190
Pucster <eugen.cocalea@gmail.com>
2009-11-30 18:12:41 +0200

    na index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include ("header.php"); ?>
-<table width="900"  border="0" cellspacing="0" cellpadding="0" align="middle" valign="middle">
+<table width="900" height=600 border="0" cellspacing="0" cellpadding="0" align="middle" valign="middle">
 <tr>
 <td bgcolor="#ff0000" rowspan="4" width="1%" style="background-image:url(images/green_frame_left.JPG);"></td>
 <td bgcolor="#ff0000" colspan="3" height="1%" style="background-image:url(images/green_frame_top.JPG);">&nbsp;</td>
